0922e3b492cf79ac088f3fd7df2449481345c52e
Daniel Escalona <daniel07escalona@gmail.com>
2021-12-15 13:21:00 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const messages = [];
+var txt;
+
+function writeCards(array, evento) {
+    for( let i = 0; i < array.length; i++) {
+        txt = `Thank you, ${array[i]}, for the wonderful ${evento} gift!`;
+        messages.push(txt);
+        console.log(txt);
+
+    }
+
+    return messages;
+}
+
+function countDown(num) {
+    while ( num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
